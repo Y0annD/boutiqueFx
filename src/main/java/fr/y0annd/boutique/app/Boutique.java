32d6fc687dbe5b98703319f6411b6a0e4bec76c9
anonymous <anonymous>
2022-11-24 09:17:47 +0100

    Fin journée 1.

--- src/main/java/fr/y0annd/boutique/app/Boutique.java
+++ src/main/java/fr/y0annd/boutique/app/Boutique.java
@@ -1,43 +1,48 @@
 package fr.y0annd.boutique.app;
 
-import fr.y0annd.boutique.metier.Configuration;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Boutique extends Application{
+public class Boutique extends Application {
 
-
-	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			// Définition de l'url correspondant au fichier FXML.
+			URL url = getClass().getClassLoader().getResource("Boutique.fxml");
+
+			// Pour l'externalisation des libellés.
+			ResourceBundle i18n = ResourceBundle.getBundle("fr.y0annd.boutique.internationalisation.boutique");
 			
-Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Boutique.fxml"));
-			
-			//La racine est placee dans la scene.
+			// Parsing du fichier FXML
+			Parent root = FXMLLoader.load(url, i18n);
+
+			// La racine est placee dans la scene.
 			Scene scene = new Scene(root);
-			
-			//On affecte une feuille de style à la scene.
+
+			// On affecte une feuille de style à la scene.
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
-			//On place la scène dans la fenetre.
+
+			// On place la scène dans la fenetre.
 			primaryStage.setScene(scene);
-			
+
 			primaryStage.setTitle("Mon titre");
-			
-			//On montre la fenetre.
+
+			// On montre la fenetre.
 			primaryStage.show();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ src/main/java/fr/y0annd/boutique/app/controller/BoutiqueController.java
@@ -0,0 +1,28 @@
+package fr.y0annd.boutique.app.controller;
+
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.layout.TilePane;
+
+import javafx.scene.control.Tab;
+
+public class BoutiqueController {
+	@FXML
+	private Label title;
+	@FXML
+	private TilePane panel;
+	@FXML
+	private Tab tabDetail;
+	@FXML
+	private Tab tabWeb;
+	@FXML
+	private Tab tabMedia;
+	@FXML
+	private Tab tabData;
+	@FXML
+	private Tab tabBackground;
+
+}
